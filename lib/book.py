0a145cc326e98cea4cda7a3d6f383dd82f3bb9fe
Anne Nabwire <nabanne96@gmail.com>
2025-04-10 18:20:20 +0300

    Lab completed

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,36 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self,title,page_count ):
+        self.title = title
+        self.page_count = page_count
+    @property
+    def title(self):
+        """The title property"""
+        return self._title
+    @title.setter
+    def title(self,title):
+        """Title must be a string"""
+        if isinstance(title, str):
+            self._title=title
+        else:
+           raise ValueError ("Title must be a string")
+    @property
+    def page_count(self):
+        """The page_count property"""
+        return self._page_count
+    @page_count.setter
+    def page_count(self, page_count):
+        """page_count must be an integer"""
+        if isinstance(page_count,int):
+            self._page_count=page_count
+        else:
+            print("page_count must be an integer")
+    
+    def turn_page(self):
+        """Method to simulate turning a page"""
+        print("Flipping the page...wow, you read fast!")
+       
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self,brand, size):
+        self.brand=brand
+        self.size=size
+        self.condition=None
+    @property
+    def size(self):
+        """Size property"""
+        return self._size
+    @size.setter
+    def size(self, size):
+        """size must be an integer"""
+        if isinstance(size, int):
+            self._size=size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        """Shoe can be repaired"""
+        self.condition="New"
+        print("Your shoe is as good as new!")
